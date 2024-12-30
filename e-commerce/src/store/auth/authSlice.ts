faf581a781313f852397615ea1269158f0a47981
nasifayc <nasifayc11@gmail.com>
2024-12-30 12:05:37 +0300

    feat: authSlice added

---
+++ e-commerce/src/store/auth/authSlice.ts
@@ -0,0 +1,40 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface User {
+  _id: string;
+  username: string;
+  email: string;
+  profile_picture: string | null;
+}
+interface AuthState {
+  user: User | null;
+  accessToken: string | null;
+}
+
+const initialState: AuthState = {
+  user: null,
+  accessToken: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuthData: (
+      state,
+      action: PayloadAction<{ user: User; accessToken: string }>
+    ) => {
+      state.user = action.payload.user;
+      state.accessToken = action.payload.accessToken;
+    },
+
+    clearAuthData: (state) => {
+      state.user = null;
+      state.accessToken = null;
+    },
+  },
+});
+
+export const { setAuthData, clearAuthData } = authSlice.actions;
+
+export default authSlice.reducer;
